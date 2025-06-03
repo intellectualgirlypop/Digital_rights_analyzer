ba7a0d2074f6f1e43ae296dec046d2c28c2beb10
Trinity Wilson <intellectualgirlypop@gmail.com>
2025-06-03 18:48:49 -0400

    Update get_tos.py
    yay pt 2

--- get_tos.py
+++ get_tos.py
@@ -1,34 +1,19 @@
 #L1
 import requests
 #L2
-from bs4 import BeautifulSoup
+import os
 #L3
-from urllib.parse import urljoin
+import anthropic
 
-TOS_PATH_NAMES = ["/terms","/terms-of-service","/legal/terms","tos","/legal","/legal/user"]
+#C1
+client = anthropic.Anthropic(api_key=os.getenv("your-api-key"))
+
 #F1
-def get_tos_url(main_url):
-    for path in TOS_PATH_NAMES:
-        #ff1.1
-        test_all_names = urljoin(main_url,path)
-        try:
-            good_response = requests.get(test_all_names,timeout=(5,10))
-        except requests.RequestException:
-            continue
-        if good_response.ok and "terms" in good_response.text.lower():
-            return test_all_names
-    return None
-#F2
 def get_text_url(url):
-    #ff2.1
+    #ff1.1
     try:
         good_response = requests.get(url, timeout=(5,10))
-        soup = BeautifulSoup(good_response.text,'html.parser')
-        paragraphs = soup.find_all(["p","li"])
-        texts = []
-        for tag in paragraphs:
-            texts.append(tag.get_text())
-        text = ' '.join(texts)
-        return text.strip()
-    except Exception as e:
+        good_response.raise_for_status()
+        return good_response.text
+    except requests.RequestException as e:
         return "Error!please try again:" + str(e)
