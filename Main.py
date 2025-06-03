6abb20686f85df8f7e36e8577be5a7f2dd0db093
Trinity Wilson <intellectualgirlypop@gmail.com>
2025-06-03 18:47:40 -0400

    Update Main.py
    jesus christ why was that so hard

--- Main.py
+++ Main.py
@@ -1,15 +1,10 @@
 import requests
-from get_tos import get_text_url,get_tos_url
+from get_tos import get_text_url
 from summarize import claude_summary
 from risk_level import risk_level
 
 def analyze_website(url):
-    tos_url = get_tos_url(url)
-    if not tos_url:
-        return "Terms of service page not found. :( "
-    print("Found ToS:" + str(tos_url))
-
-    tos_text = get_text_url(tos_url)
+    tos_text = get_text_url(url)
     if "Error" in tos_text:
         return tos_text
     print("Loading Summary ...")
@@ -19,15 +14,14 @@ def analyze_website(url):
     rating = risk_level(tos_text)
 
     return{
-        "tos_url":tos_url,
         "summary":summary,
         "rating": rating
     }
 
 if __name__ == "__main__":
-    output = analyze_website("https://www.linkedin.com")
+    output = analyze_website("https://www.linkedin.com/legal/user-agreement")
 
     print("\n---Summary---")
-    print(output+"summary")
+    print(output["summary"])
     print("\n---Risk Level---")
-    print(output+"rating")
+    print(output["rating"])
